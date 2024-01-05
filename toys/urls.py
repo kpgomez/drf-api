f981d0f53e97b20c7f432211431b7e0bf2f40697
Kiengchay Gomez <kphoutthavong@gmail.com>
2024-01-04 22:19:55 -0600

    modified provided unit tests; all tests are passing

--- toys/urls.py
+++ toys/urls.py
@@ -3,6 +3,6 @@ from .views import ToyList, ToyDetail
 
 
 urlpatterns = [
-    path("", ToyList.as_view()),
-    path("<int:pk>/", ToyDetail.as_view()),
+    path("", ToyList.as_view(), name="toy_list"),
+    path("<int:pk>/", ToyDetail.as_view(), name="toy_detail"),
 ]
